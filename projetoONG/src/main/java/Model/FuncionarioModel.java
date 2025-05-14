b56d3d491090304d9e82b927d463abf14adcd1eb
ALUNO <fernandesannakarolina08@gmail.com>
2025-05-14 17:53:13 -0300

    Classe de funcionarios e voluntarios

---
+++ projetoONG/src/main/java/Model/FuncionarioModel.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class FuncionarioModel {
+    String nome;
+    String cpf;
+    String dataNascimento;
+    String sexo;
+    String email;
+    String senha;
+   
+
+    public FuncionarioModel(String nome, String cpf, String dataNascimento, String sexo, String email, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public FuncionarioModel() {
+    }
+    
+    
+    
+    
+    
+    
+}
