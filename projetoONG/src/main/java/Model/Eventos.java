7e71e8526a5653e7a6f0c0abdb795feaa84f46b6
ALUNO <ALUNO@DESKTOP-5EKBLTB>
2025-05-14 17:52:41 -0300

    Criação de eventos(tela)

---
+++ projetoONG/src/main/java/Model/Eventos.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class Eventos {
+    String nome;
+    String data;
+    String horario;
+    String duracao;
+
+    public Eventos(String nome, String data, String horario, String duracao) {
+        this.nome = nome;
+        this.data = data;
+        this.horario = horario;
+        this.duracao = duracao;
+    }
+
+    public Eventos() {
+    }
+    
+    
+    
+}
--- projetoONG/src/main/java/View/Relatorios.java
+++ projetoONG/src/main/java/View/Relatorios.java
@@ -73,15 +73,15 @@ public class Relatorios extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(194, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(109, 109, 109)
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(158, 158, 158))
+                .addContainerGap(243, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(95, 95, 95)
+                .addGap(79, 79, 79)
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
