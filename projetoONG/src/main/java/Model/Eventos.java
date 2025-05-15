3d92d0ccedb21067b061cbe55426793f8050ee65
ALUNO <ALUNO@DESKTOP-5EKBLTB>
2025-05-15 15:08:45 -0300

    Telas

--- projetoONG/src/main/java/Model/Eventos.java
+++ projetoONG/src/main/java/Model/Eventos.java
@@ -9,10 +9,10 @@ package Model;
  * @author ALUNO
  */
 public class Eventos {
-    String nome;
-    String data;
-    String horario;
-    String duracao;
+     private String nome;
+     private String data;
+     private String horario;
+     private String duracao;
 
     public Eventos(String nome, String data, String horario, String duracao) {
         this.nome = nome;
@@ -23,6 +23,43 @@ public class Eventos {
 
     public Eventos() {
     }
+    
+    // metodos getter e setter
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+    
+    
+    
     
     
     
